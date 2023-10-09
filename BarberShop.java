bfef4ea5e9cc50eae705fd73ba37def12f60c334
Jordi Castro <jordi.castro003@gmail.com>
2023-10-09 16:19:52 -0500

    .

--- BarberShop.java
+++ BarberShop.java
@@ -15,6 +15,97 @@ import java.util.ArrayList;
  *      - Customers need to enter the waiting room before accessing Barber's room
  */
 
+public class BarberShop 
+{
+    private static int elapsedTime = 0;
+    private static final int NUM_BARBERS = 5; 
+    private static final int NUM_CHAIRS= NUM_BARBERS * 2;
+    //private static final int NUM_CHAIRS= NUM_BARBERS * 2;
+
+
+    public static void main(String[] args) 
+    {
+        // Default values
+        int sleepTime = 20;
+        int numBarber = 5;
+        int numCustomer = numBarber+ numBarber*2;
+
+
+        Semaphore swait = new Semaphore(NUM_CHAIRS);
+        Semaphore sbarber = new Semaphore(NUM_BARBERS);
+        Semaphore mwait = new Semaphore(1);
+        Semaphore mbarber = new Semaphore(1);
+
+        if (args.length == 2) 
+        {
+            sleepTime = Integer.parseInt(args[0]);
+            numBarber = Integer.parseInt(args[1]);
+            System.out.printf("Sleep time = %d\t", sleepTime);
+            System.out.printf("Number of Barber = %d\t", numBarber);
+            System.out.println("");
+        } 
+        else 
+        {
+            System.out.println("Usage: <sleep time> <no. Barber>");
+            System.out.println("Using default");
+            System.out.printf("Sleep time = %d\t", sleepTime);
+            System.out.printf("Number of Barber = %d\t", numBarber);
+            System.out.println("");
+        }
+
+	    // Initialize/declare variables including object of class type Customer, mutexes, and semaphores
+
+        // Time info
+        long startTime = System.currentTimeMillis();
+
+
+	    // This is one way but there are others.
+        List<Thread> customerThreads = new ArrayList<>();
+
+        for (int i = 0; i < numCustomer; i++) 
+        {
+            if (elapsedTime < sleepTime) 
+            {
+
+		        // Instantiate customers and don't forget to pass in mutexes and semaphores
+                Customer myCustomer = new Customer(i, swait, sbarber, mwait, mbarber);
+
+		        // Create a thread
+                Thread c = new Thread(myCustomer);
+
+		        // Add the thread to a list IF you choose to do it that way
+                customerThreads.add(c);
+
+		        // Start up the threads that will run code in run()
+                c.start();
+
+                myCustomer.setThread(c); // Set the thread reference in Customer class
+
+                customerThreads.add(c); // Add the thread to the list
+
+            } else 
+            {
+                System.out.println("Shop is closed. No new customers allowed.");
+                break;
+            }
+            elapsedTime = (int) (System.currentTimeMillis() - startTime);
+        }
+
+        // Wait for all customer threads to finish
+        for (Thread customerThread : customerThreads) 
+        {
+            try 
+            {
+                customerThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("All customers have been served. Closing the shop.");
+    }
+}
+
 class Customer implements Runnable 
 {
     private int id;
@@ -47,8 +138,16 @@ class Customer implements Runnable
     private void getWait() 
     {
          //Use mutexes and semaphores here
+         if (isFull())
+         {
+            // leave!
+         }
+         else
+         {
+            this.numInWait++;
+         }
          //Do not forget about using try and catch.
-	 //code ...
+	     //code ...
     }
 
 
@@ -59,7 +158,7 @@ class Customer implements Runnable
     private void getBarberChair() 
     {
         //Use mutexes and semaphores appropriately 
-	//code ...
+	    //code ...
 
         this.numInBarber++;
         this.numInwait--;
@@ -67,7 +166,7 @@ class Customer implements Runnable
         System.out.printf("\t\tCustomer %d enters Barbers chair. We have %d performing haircut and %d waiting\n", this.id, this.numInBarber, this.numInwait);
 
         //Do not forget to signal the appropriate mutexes and semaphores
-	//code ...
+	    //code ...
     }
 
     /**
@@ -77,10 +176,10 @@ class Customer implements Runnable
     private void exitShop() 
     {
         // Use mutexes and semaphores appropriately
-	// code ...
+	    // code ...
         this.numInBarber--;
         //Do not forget to signal the appropriate mutexes and semaphores
-	// code ...
+	    // code ...
     }
 
     @Override
@@ -88,26 +187,29 @@ class Customer implements Runnable
     {
 
         //Sleep a random amount of time here
-	// code ...
+        sleeps();
+	    // code ...
 
 	
 
         System.out.printf("Customer %d enters the shop\n", id);
 
-	// Call method to get access to Waiting Room
+	    // Call method to get access to Waiting Room
         getWait();
 
         // Check if the thread is interrupted (customer leaves in frustration)
         if (!Thread.currentThread().isInterrupted()) 
         {
 
-	    // Now compete for a barber chair
+	        // Now compete for a barber chair
             getBarberChair();
 
-	    // Sleep a random amount of time her
-	    // code ...
+	        // Sleep a random amount of time here
+            sleeps();
+
+	        // code ...
             
-	    // Exit
+	        // Exit
             exitShop();
 
             System.out.printf("\t\t\tCustomer %d exits the system\n", id);
@@ -116,85 +218,18 @@ class Customer implements Runnable
         
     }
 
-}
-
-
-public class BarberShop 
-{
-    private static int elapsedTime = 0;
-
-    public static void main(String[] args) 
+    public void sleeps()
     {
-        // Default values
-        int sleepTime = 20;
-        int numBarber = 5;
-
-        if (args.length == 2) 
-        {
-            sleepTime = Integer.parseInt(args[0]);
-            numBarber = Integer.parseInt(args[1]);
-            System.out.printf("Sleep time = %d\t", sleepTime);
-            System.out.printf("Number of Barber = %d\t", numBarber);
-            System.out.println("");
-        } 
-        else 
-        {
-            System.out.println("Usage: <sleep time> <no. Barber>");
-            System.out.println("Using default");
-            System.out.printf("Sleep time = %d\t", sleepTime);
-            System.out.printf("Number of Barber = %d\t", numBarber);
-            System.out.println("");
-        }
-
-	// Initialize/declare variables including object of class type Customer, mutexes, and semaphores
-
-        // Time info
-        long startTime = System.currentTimeMillis();
-
-
-	// This is one way but there are others.
-        List<Thread> customerThreads = new ArrayList<>();
-
-        for (int i = 0; i < numCustomer; i++) 
-        {
-            if (elapsedTime < sleepTime) 
+            try()
             {
-
-		// Instantiate customers and don't forget to pass in mutexes and semaphores
-                myCustomer = new Customer(i, swait, sbarber, mwait, mbarber);
-
-		// Create a thread
-                Thread c = new Thread(myCustomer);
-
-		// Add the thread to a list IF you choose to do it that way
-                customerThreads.add(c);
-
-		// Start up the threads that will run code in run()
-                c.start();
-
-                myCustomer.setThread(c); // Set the thread reference in Customer class
-
-                customerThreads.add(c); // Add the thread to the list
-
-            } else 
-            {
-                System.out.println("Shop is closed. No new customers allowed.");
-                break;
+                double randSleepTime = Math.random();
+                long miliSleepTime = (long) (randSleepTime *1000);
+                Thread.sleep(miliSleepTime);
             }
-            elapsedTime = (int) (System.currentTimeMillis() - startTime);
-        }
-
-        // Wait for all customer threads to finish
-        for (Thread customerThread : customerThreads) 
-        {
-            try 
+            catch(InterruptedException e)
             {
-                customerThread.join();
-            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
-
-        System.out.println("All customers have been served. Closing the shop.");
     }
+
 }
